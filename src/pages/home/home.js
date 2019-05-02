7a5201870cd74a13cedb20099c4bd426da150666
unknown <juradomartinbenedict@gmail.com>
2019-05-02 13:24:55 -0400

    modified responsive, deleted lock

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,18 +3,22 @@ import Header from "../../components/header";
 import Work from "../work/index";
 import About from "../about/index";
 import Skills from "../skills/index";
-
+import Responsive from "react-responsive";
 import "./style.css";
+
+const Default = props => <Responsive {...props} minWidth={768} />;
 
 function Main() {
   return (
     <div>
-      <div className="header">
-        <Header />
-        <Work />
-        <About />
-        <Skills />
-      </div>
+      <Default>
+        <div className="header">
+          <Header />
+          <Work />
+          <About />
+          <Skills />
+        </div>
+      </Default>
     </div>
   );
 }
