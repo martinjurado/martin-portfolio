3e36f334e7f76d694223b831dd4c1c04b2683886
unknown <juradomartinbenedict@gmail.com>
2019-05-13 08:59:53 -0400

    added credentials

--- index.js
+++ index.js
@@ -7,7 +7,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/api/form', (req, res) => {
-    console.log(req.body);
+    nodemailer.createTestAccount((err, account) => {
+        
+        const htmlEmail = `
+        <h3> Contact Details </h3>
+        <ul>
+        <li>Name: ${req.body.name} </li>
+        <li>Email: ${req.body.email} </li>
+        </ul>
+        <h3> Message </h3> 
+        <p> ${req.body.message} </p>
+        `
+
+        let transporter = nodemailer.createTransport({
+            service: "Gmail",
+            port: 587,
+            auth: {
+                user: 'juradomartinbenedict@gmail.com',
+                pass: 'mbvj1127'
+            }
+        })
+
+        let mailOptions = {
+            from: "test@testaccount.com",
+            to: 'zetta.little96@ethereal.email',
+            replyTo: "test@testaccount.com",
+            subject: "New Message",
+            text: req.body.message,
+            html: htmlEmail
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                return console.log(err)
+            }
+            console.log("Message sent: %s", info.message)
+            console.log("Message URL: %s", nodemailer.getTestMessageUrl)
+        })
+    })
 })
 
 const PORT = process.env.PORT || 3001
