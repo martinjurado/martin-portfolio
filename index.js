78393ba725e4e5478aae87ff12d764ac7c084450
unknown <juradomartinbenedict@gmail.com>
2019-05-15 18:14:07 -0400

    added changes

--- index.js
+++ index.js
@@ -3,26 +3,21 @@ const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 
 const path = require("path");
-const creds = require("./creds")
+const creds = require("./creds");
 const app = express();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/public'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
+// app.get("*", (request, response) => {
+//   response.sendFile(path.join(__dirname, "client/public", "index.html"));
+// });
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/public', 'index.html'));
-});
-
-
-app.post('/api/form', (req, res) => {
-    nodemailer.createTestAccount((err, account) => {
-        
-        const htmlEmail = `
+app.post("/api/form", (req, res) => {
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
         <h3> Contact Details </h3>
         <ul>
         <li>Name: ${req.body.name} </li>
@@ -30,38 +25,38 @@ app.post('/api/form', (req, res) => {
         </ul>
         <h3> Message </h3> 
         <p> ${req.body.message} </p>
-        `
+        `;
 
-        let transporter = nodemailer.createTransport({
-            service: "Gmail",
-            port: 587,
-            auth: {
-                user: creds.USER,
-                pass: creds.PASS
-            }
-        })
+    let transporter = nodemailer.createTransport({
+      service: "Gmail",
+      port: 587,
+      auth: {
+        user: creds.USER,
+        pass: creds.PASS
+      }
+    });
 
-        let mailOptions = {
-            from: "test@testaccount.com",
-            to: 'zetta.little96@ethereal.email',
-            replyTo: "test@testaccount.com",
-            subject: "New Message",
-            text: req.body.message,
-            html: htmlEmail
-        }
+    let mailOptions = {
+      from: "test@testaccount.com",
+      to: "zetta.little96@ethereal.email",
+      replyTo: "test@testaccount.com",
+      subject: "New Message",
+      text: req.body.message,
+      html: htmlEmail
+    };
 
-        transporter.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                return console.log(err)
-            }
-            console.log("Message sent: %s", info.message)
-            console.log("Message URL: %s", nodemailer.getTestMessageUrl(info))
-        })
-    })
-})
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Message sent: %s", info.message);
+      console.log("Message URL: %s", nodemailer.getTestMessageUrl(info));
+    });
+  });
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
